513008d02fb51256004c7986924d667cd66758a7
Sungyeon <csy357@gmail.com>
2025-05-12 19:36:44 +0900

    [refactor] 안쓰는 import 삭제

--- src/main/java/kr/co/pawong/pwbe/user/config/SecurityConfig.java
+++ src/main/java/kr/co/pawong/pwbe/user/config/SecurityConfig.java
@@ -20,7 +20,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.ExceptionTranslationFilter;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Slf4j
 @Configuration
