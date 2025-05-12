dce36700ed3efe68874f67bebadcfcbe66779ebd
Sungyeon <csy357@gmail.com>
2025-05-12 19:32:37 +0900

    [feat] 축종코드 enum에 upKindCd값을 value로 추가

--- src/main/java/kr/co/pawong/pwbe/adoption/enums/UpKindNm.java
+++ src/main/java/kr/co/pawong/pwbe/adoption/enums/UpKindNm.java
@@ -6,7 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Getter
 public enum UpKindNm {
-    개,
-    고양이,
-    기타;
+    개("417000"),
+    고양이("422400"),
+    기타("429900");
+
+    private final String value;
+    UpKindNm(String value) {
+        this.value = value;
+    }
 }
