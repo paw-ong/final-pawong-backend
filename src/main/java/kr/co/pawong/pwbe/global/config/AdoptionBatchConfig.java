d60b754a8c293c408328bfa1435a02d8087c3809
h0725j <guswldpqj1@naver.com>
2025-05-12 15:00:09 +0900

    [feat] 배치 스케쥴링 구현 + 메세지 컨버터 수정

---
+++ src/main/java/kr/co/pawong/pwbe/global/config/AdoptionBatchConfig.java
@@ -0,0 +1,37 @@
+package kr.co.pawong.pwbe.global.config;
+
+import kr.co.pawong.pwbe.adoption.application.port.in.ApiRequestUseCase;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@EnableAutoConfiguration
+public class AdoptionBatchConfig {
+
+    @Bean
+    public Job adoptionApiJob(JobRepository jobRepository, Step adoptionApiStep) {
+        return new JobBuilder("adoptionApiJob", jobRepository)
+                .start(adoptionApiStep)
+                .build();
+    }
+
+    @Bean
+    public Step adoptionApiStep(JobRepository jobRepository, PlatformTransactionManager transactionManager,
+            ApiRequestUseCase apiRequestUseCase) {
+        return new StepBuilder("adoptionApiStep", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    apiRequestUseCase.fetchAndSaveAdoptions();
+                    return RepeatStatus.FINISHED; // 한 번만 실행
+                }, transactionManager)
+                .build();
+    }
+}
+
