40f3aafff02f1a51e45e11ab8cc88e1fb2478dfd
Dingwoonee <slm0523@naver.com>
2025-05-13 18:18:29 +0900

    [test] TimeUtilsTest 수정

--- src/test/java/kr/co/pawong/pwbe/global/time/TimeUtilsTest.java
+++ src/test/java/kr/co/pawong/pwbe/global/time/TimeUtilsTest.java
@@ -13,7 +13,7 @@ class TimeUtilsTest {
 
     private TimeUtils utils;
     private static final LocalDateTime FIXED_LDT =
-            LocalDateTime.of(2025, 5, 13, 12, 0, 0);
+            LocalDateTime.of(2025, 5, 13, 12, 14, 3);
     private static final LocalDate FIXED_LD =
             LocalDate.of(2025, 5, 13);
 
@@ -25,21 +25,65 @@ class TimeUtilsTest {
     }
 
     @Test
-    void 시간_포맷팅_초_테스트() {
+    void 시간_포맷팅_테스트() {
+        // given
+        LocalDateTime dt = LocalDateTime.of(2025, 5, 13, 12, 12, 2);
+        // when
+        String strTime = TimeUtils.formatTime(dt);
+        // then
+        assertThat(strTime)
+                .isEqualTo("2025-05-13 12:12:02");
+    }
+
+    @Test
+    void 시간_포맷팅_null_입력_시_공백_반환() {
+        // given
+        LocalDateTime dt = null;
+        // when
+        String strTime = TimeUtils.formatTime(dt);
+        // then
+        assertThat(strTime)
+                .isEqualTo("");
+    }
+
+    @Test
+    void 날짜_포맷팅_테스트() {
+        // given
+        LocalDate dt = LocalDate.of(2025, 5, 13);
+        // when
+        String strTime = TimeUtils.formatDate(dt);
+        // then
+        assertThat(strTime)
+                .isEqualTo("2025-05-13");
+    }
+
+    @Test
+    void 날짜_포맷팅_null_입력_시_공백_반환() {
+        // given
+        LocalDate dt = null;
+        // when
+        String strTime = TimeUtils.formatDate(dt);
+        // then
+        assertThat(strTime)
+                .isEqualTo("");
+    }
+
+    @Test
+    void 시간_한글_포맷팅_초_테스트() {
         LocalDateTime fiveSecondsAgo = FIXED_LDT.minusSeconds(5);
         assertThat(utils.formatTimeAgo(fiveSecondsAgo))
                 .isEqualTo("5초 전");
     }
 
     @Test
-    void 시간_포맷팅_분_테스트() {
+    void 시간_한글_포맷팅_분_테스트() {
         LocalDateTime tenMinutesAgo = FIXED_LDT.minusMinutes(10);
         assertThat(utils.formatTimeAgo(tenMinutesAgo))
                 .isEqualTo("10분 전");
     }
 
     @Test
-    void 시간_포맷팅_년_테스트() {
+    void 시간_한글_포맷팅_년_테스트() {
         LocalDateTime twoYearsAgo = FIXED_LDT.minusYears(2);
         assertThat(utils.formatTimeAgo(twoYearsAgo))
                 .isEqualTo("2년 전");
@@ -47,28 +91,28 @@ class TimeUtilsTest {
 
 
     @Test
-    void 날_수_포맷팅_당일_테스트() {
+    void 날_수_한글_포맷팅_당일_테스트() {
         LocalDate today = FIXED_LD;
         assertThat(utils.formatDateAgo(today))
                 .isEqualTo("오늘");
     }
 
     @Test
-    void 날_수_포맷팅_일_테스트() {
+    void 날_수_한글_포맷팅_일_테스트() {
         LocalDate fiveDaysAgo = FIXED_LD.minusDays(5);
         assertThat(utils.formatDateAgo(fiveDaysAgo))
                 .isEqualTo("5일 전");
     }
 
     @Test
-    void 날_수_포맷팅_월_테스트() {
+    void 날_수_한글_포맷팅_월_테스트() {
         LocalDate threeMonthsAgo = FIXED_LD.minusMonths(3);
         assertThat(utils.formatDateAgo(threeMonthsAgo))
                 .isEqualTo("3개월 전");
     }
 
     @Test
-    void 날_수_포맷팅_년_테스트() {
+    void 날_수_한글_포맷팅_년_테스트() {
         LocalDate twoYearsAgo = FIXED_LD.minusYears(2);
         assertThat(utils.formatDateAgo(twoYearsAgo))
                 .isEqualTo("2년 전");
