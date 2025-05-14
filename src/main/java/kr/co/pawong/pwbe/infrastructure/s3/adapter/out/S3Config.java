0b81894869ed23393b6be75d7aed38e530271561
Sungyeon <csy357@gmail.com>
2025-05-14 16:45:18 +0900

    [feat] aws s3 config

---
+++ src/main/java/kr/co/pawong/pwbe/infrastructure/s3/adapter/out/S3Config.java
@@ -0,0 +1,49 @@
+package kr.co.pawong.pwbe.infrastructure.s3.adapter.out;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Slf4j
+@Configuration
+public class S3Config {
+
+    @Value("${cloud.aws.s3.accessKey}")
+    private String accessKey;
+    @Value("${cloud.aws.s3.secretKey}")
+    private String secretKey;
+
+    @Bean
+    public S3Client amazonS3() {
+        log.info("S3Client: {}", accessKey);
+        return S3Client.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)
+                        )
+                )
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        log.info("S3Presigner: {}", accessKey);
+        return S3Presigner.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)
+                        )
+                )
+                .build();
+    }
+
+}
