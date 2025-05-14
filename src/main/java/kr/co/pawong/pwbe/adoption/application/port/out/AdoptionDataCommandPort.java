f57599d83a62b986eba3cd70f8c952903d4e4232
h0725j <guswldpqj1@naver.com>
2025-05-14 16:45:44 +0900

    [refactor] 사용하지 않는 메서드 삭제

--- src/main/java/kr/co/pawong/pwbe/adoption/application/port/out/AdoptionDataCommandPort.java
+++ src/main/java/kr/co/pawong/pwbe/adoption/application/port/out/AdoptionDataCommandPort.java
@@ -4,13 +4,9 @@ import java.util.List;
 import kr.co.pawong.pwbe.adoption.domain.model.Adoption;
 
 public interface AdoptionDataCommandPort {
-    // 유기동물정보 RDB에 저장
-    void saveAdoptions(List<Adoption> adoptions);
 
-    // EmbeddingDone 업데이트
-    void updateIsEmbedded(List<Adoption> adoptions);
+    void updateIsEmbeddedByIds(List<Long> ids);
 
-    // Ai 관련 데이터 업데이트
     void updateAiFields(List<Adoption> adoptions);
 
     void updateAdoption(Adoption adoption);
