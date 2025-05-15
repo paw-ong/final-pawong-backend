02ae0947850882cbf15d51dd51e5a79c5b4ce340
yongje <dldydwp9024@gmail.com>
2025-05-15 15:05:00 +0900

    [feat] 무한스크롤 페이지를 위한 dto

---
+++ src/main/java/kr/co/pawong/pwbe/lostPost/application/port/in/dto/SliceLostPostSearchResponses.java
@@ -0,0 +1,10 @@
+package kr.co.pawong.pwbe.lostPost.application.port.in.dto;
+
+import java.util.List;
+
+public class SliceLostPostSearchResponses {
+
+    private boolean hasNext; // 다음 페이지가 있는지 여부
+    private List<LostPostCard> lostPostCards;
+
+}
