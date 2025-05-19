fd7dd95be474a06dce5622affd8bf2045872845b
Dingwoonee <slm0523@naver.com>
2025-05-19 15:08:44 +0900

    [feat] 콘솔 출력과 파일 로그를 구별

--- src/main/java/kr/co/pawong/pwbe/global/filter/TracingFilter.java
+++ src/main/java/kr/co/pawong/pwbe/global/filter/TracingFilter.java
@@ -8,13 +8,11 @@ import java.io.IOException;
 import java.util.Optional;
 import java.util.UUID;
 import org.slf4j.MDC;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-@Profile(value = {"prod", "dev"})
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class TracingFilter extends OncePerRequestFilter {
