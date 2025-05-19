101227dcf68b41c04a15c5fa9fb37733fec43090
Dingwoonee <slm0523@naver.com>
2025-05-19 12:26:31 +0900

    [feat] dev와 prod에만 traceId가 나오게 함

--- src/main/java/kr/co/pawong/pwbe/global/filter/TracingFilter.java
+++ src/main/java/kr/co/pawong/pwbe/global/filter/TracingFilter.java
@@ -8,11 +8,13 @@ import java.io.IOException;
 import java.util.Optional;
 import java.util.UUID;
 import org.slf4j.MDC;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@Profile(value = {"prod", "dev"})
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class TracingFilter extends OncePerRequestFilter {
@@ -28,7 +30,6 @@ public class TracingFilter extends OncePerRequestFilter {
         String traceId = Optional.ofNullable(req.getHeader("X-Request-ID"))
                 .orElse(UUID.randomUUID().toString());
         MDC.put("traceId", traceId);
-        logger.info("traceId: " + traceId);
         res.setHeader("X-Request-ID", traceId);
         try {
             chain.doFilter(req, res);
