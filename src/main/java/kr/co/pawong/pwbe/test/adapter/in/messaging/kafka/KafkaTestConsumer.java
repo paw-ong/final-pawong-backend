93fed09881ec9b0b0aab703f0c0f1fd3dc1dbd61
Dingwoonee <slm0523@naver.com>
2025-05-24 16:52:14 +0900

    [fix] objectMapper를 활용하게 로직 수정

--- src/main/java/kr/co/pawong/pwbe/test/adapter/in/messaging/kafka/KafkaTestConsumer.java
+++ src/main/java/kr/co/pawong/pwbe/test/adapter/in/messaging/kafka/KafkaTestConsumer.java
@@ -1,6 +1,8 @@
 package kr.co.pawong.pwbe.test.adapter.in.messaging.kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.co.pawong.pwbe.test.adapter.in.messaging.kafka.dto.TestMessageConsumeDto;
 import kr.co.pawong.pwbe.test.application.port.in.PrintLogUseCase;
 import kr.co.pawong.pwbe.global.config.KafkaTopicConfig;
 import lombok.RequiredArgsConstructor;
@@ -20,31 +22,45 @@ public class KafkaTestConsumer {
      */
     @KafkaListener(topics = KafkaTopicConfig.TEST_TOPIC, groupId = "test-group1")
     public void listen1_1(String message) {
-        printLogUseCase.printLog("Received message test1-1: " + message);
+        TestMessageConsumeDto dto = getValue(message);
+        printLogUseCase.printLog("Received message test1-1: " + dto);
     }
 
     @KafkaListener(topics = KafkaTopicConfig.TEST_TOPIC, groupId = "test-group1")
     public void listen1_2(String message) {
-        printLogUseCase.printLog("Received message test1-2: " + message);
+        TestMessageConsumeDto dto = getValue(message);
+        printLogUseCase.printLog("Received message test1-2: " + dto);
     }
 
     @KafkaListener(topics = KafkaTopicConfig.TEST_TOPIC, groupId = "test-group2")
     public void listen2_1(String message) {
-        printLogUseCase.printLog("Received message test2-1: " + message);
+        TestMessageConsumeDto dto = getValue(message);
+        printLogUseCase.printLog("Received message test2-1: " + dto);
     }
 
     @KafkaListener(topics = KafkaTopicConfig.TEST_TOPIC, groupId = "test-group2")
     public void listen2_2(String message) {
-        printLogUseCase.printLog("Received message test2-2: " + message);
+        TestMessageConsumeDto dto = getValue(message);
+        printLogUseCase.printLog("Received message test2-2: " + dto);
     }
 
     @KafkaListener(topics = KafkaTopicConfig.TEST_TOPIC)
     public void listenInDefaultGroup1(String message) {
-        printLogUseCase.printLog("Received message default-1: " + message);
+        TestMessageConsumeDto dto = getValue(message);
+        printLogUseCase.printLog("Received message default-1: " + dto);
     }
 
     @KafkaListener(topics = KafkaTopicConfig.TEST_TOPIC)
     public void listenInDefaultGroup2(String message) {
-        printLogUseCase.printLog("Received message default-2: " + message);
+        TestMessageConsumeDto dto = getValue(message);
+        printLogUseCase.printLog("Received message default-2: " + dto);
+    }
+
+    private TestMessageConsumeDto getValue(String message) {
+        try {
+            return objectMapper.readValue(message, TestMessageConsumeDto.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("[KafkaTestConsumer DTO 변환 에러]", e);
+        }
     }
 }
