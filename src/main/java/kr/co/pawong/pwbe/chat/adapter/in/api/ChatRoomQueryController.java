13874cb93ac754c0c7c6f26b3104aaac6f5b47d5
Yoo Hyunseok <brianyhs@naver.com>
2025-05-26 15:34:05 +0900

    [feat] 유저가 채팅방에 존재하는지 확인하는 api

--- src/main/java/kr/co/pawong/pwbe/chat/adapter/in/api/ChatRoomQueryController.java
+++ src/main/java/kr/co/pawong/pwbe/chat/adapter/in/api/ChatRoomQueryController.java
@@ -1,6 +1,7 @@
 package kr.co.pawong.pwbe.chat.adapter.in.api;
 
 import java.util.List;
+import kr.co.pawong.pwbe.chat.adapter.in.api.dto.response.ChatRoomContainsUserResponse;
 import kr.co.pawong.pwbe.chat.adapter.in.api.dto.response.ChatRoomsResponse;
 import kr.co.pawong.pwbe.chat.application.port.in.QueryChatRoomDataUseCase;
 import kr.co.pawong.pwbe.chat.application.port.in.dto.ChatRoomDetail;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +25,11 @@ public class ChatRoomQueryController {
 
     private final QueryChatRoomDataUseCase queryChatDataUseCase;
 
+    /**
+     * 유저의 채팅방 목록을 조회하는 api
+     * @param userDetails
+     * @return List<ChatRoomDetail>
+     */
     @GetMapping("/rooms")
     public ResponseEntity<ChatRoomsResponse> findChatRooms(
             @AuthenticationPrincipal CustomUserDetails userDetails
@@ -32,5 +39,23 @@ public class ChatRoomQueryController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(new ChatRoomsResponse(allChatRooms));
+    }
+
+    /**
+     * 유저가 채팅방에 존재하는지 확인하는 api
+     * @param roomId
+     * @param userDetails
+     * @return boolean (존재 여부)
+     */
+    @GetMapping("/rooms/{roomId}")
+    public ResponseEntity<ChatRoomContainsUserResponse> checkUserInChatRoom(
+            @PathVariable Long roomId,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        Long userId = userDetails.getUserId();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ChatRoomContainsUserResponse(
+                        queryChatDataUseCase.userExistsInChatRoom(userId, roomId)));
     }
 }
