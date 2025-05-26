16369633391b68c20b0aacd675e32f97b02da9e3
Sungyeon <csy357@gmail.com>
2025-05-26 14:30:00 +0900

    [feat] chat 메세지 저장, 구독채널에 전송

---
+++ src/main/java/kr/co/pawong/pwbe/chat/adapter/in/api/ChatMessageCommandController.java
@@ -0,0 +1,44 @@
+package kr.co.pawong.pwbe.chat.adapter.in.api;
+
+import kr.co.pawong.pwbe.chat.adapter.in.api.dto.request.ChatMessageCreateRequest;
+import kr.co.pawong.pwbe.chat.adapter.in.api.dto.response.ChatMessageResponse;
+import kr.co.pawong.pwbe.chat.application.port.in.CommandChatMessageDataUseCase;
+import kr.co.pawong.pwbe.user.adapter.out.security.CustomUserDetails;
+import kr.co.pawong.pwbe.user.application.port.in.QueryNicknameUseCase;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ChatMessageCommandController {
+
+    private final CommandChatMessageDataUseCase commandChatMessageDataUseCase;
+    private final QueryNicknameUseCase queryNicknameUseCase;
+
+    @MessageMapping("/chat.send/{roomId}")
+    @SendToUser("/queue/chat/{roomId}")
+    public ChatMessageResponse sendMessage(
+            @Payload ChatMessageCreateRequest request,
+            @DestinationVariable Long roomId,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        Long chatMessageId = commandChatMessageDataUseCase.createChatMessage(
+                roomId,
+                userDetails.getUserId(),
+                request.getContent());
+        return new ChatMessageResponse(
+                chatMessageId,
+                request.getContent(),
+                userDetails.getUserId(),
+                queryNicknameUseCase.getNicknameByUserId(userDetails.getUserId()),
+                request.getCreateAt()
+        );
+    }
+}
