569ead871b94ece66ee1b67c3ba4479af8dbff0a
h0725j <guswldpqj1@naver.com>
2025-05-26 17:34:11 +0900

    [refactor] Kafka 관련 yml 이동

--- src/main/java/kr/co/pawong/pwbe/infrastructure/messaging/application/port/out/MessagePublishPort.java
+++ src/main/java/kr/co/pawong/pwbe/infrastructure/messaging/application/port/out/MessagePublishPort.java
@@ -1,6 +1,10 @@
 package kr.co.pawong.pwbe.infrastructure.messaging.application.port.out;
 
+import kr.co.pawong.pwbe.Notification.application.service.dto.NotificationDto;
+
 public interface MessagePublishPort {
 
     void publishMessage(String topic, Object message);
+
+    void publishFcmNotificationMessage(NotificationDto notificationDto);
 }
