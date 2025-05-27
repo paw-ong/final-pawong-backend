11a599db103ea445fe1f05cfc4e8c402fcabd08f
h0725j <guswldpqj1@naver.com>
2025-05-27 10:17:57 +0900

    [feat] FcmTokenEntity 수정

--- src/main/java/kr/co/pawong/pwbe/infrastructure/fcm/adapter/persistence/jpa/entity/FcmTokenEntity.java
+++ src/main/java/kr/co/pawong/pwbe/infrastructure/fcm/adapter/persistence/jpa/entity/FcmTokenEntity.java
@@ -1,6 +1,5 @@
 package kr.co.pawong.pwbe.infrastructure.fcm.adapter.persistence.jpa.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,22 +16,18 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "fcm_tokens")
+@Table(name = "Fcm_tokens")
 public class FcmTokenEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user_id", nullable = false)
     private Long userId;
 
-    @Column(name = "token", nullable = false)
     private String token;
 
-    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
-    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
 }
