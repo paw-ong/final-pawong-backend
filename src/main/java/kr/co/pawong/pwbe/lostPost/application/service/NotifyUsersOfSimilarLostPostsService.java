6bfb5b8fad382e2aab9b7238668aaf64ce3e1d91
h0725j <guswldpqj1@naver.com>
2025-05-27 14:13:50 +0900

    [feat] LostAnimal es 저장 구현

--- src/main/java/kr/co/pawong/pwbe/lostPost/application/service/NotifyUsersOfSimilarLostPostsService.java
+++ src/main/java/kr/co/pawong/pwbe/lostPost/application/service/NotifyUsersOfSimilarLostPostsService.java
@@ -1,13 +1,25 @@
 package kr.co.pawong.pwbe.lostPost.application.service;
 
 import kr.co.pawong.pwbe.lostPost.application.port.in.NotifyUsersOfSimilarLostPostsUseCase;
+import kr.co.pawong.pwbe.lostPost.application.port.out.LostAdoptionDataQueryPort;
+import kr.co.pawong.pwbe.lostPost.application.port.out.LostAnimalEngineCommandPort;
+import kr.co.pawong.pwbe.lostPost.application.port.out.LostPostDataQueryPort;
+import kr.co.pawong.pwbe.lostPost.application.port.out.dto.LostAnimalDto;
+import kr.co.pawong.pwbe.lostPost.domain.LostAdoption;
+import kr.co.pawong.pwbe.lostPost.domain.LostPost;
 import kr.co.pawong.pwbe.lostPost.enums.PostType;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class NotifyUsersOfSimilarLostPostsService implements NotifyUsersOfSimilarLostPostsUseCase {
+
+    private final LostPostDataQueryPort lostPostDataQueryPort;
+    private final LostAdoptionDataQueryPort lostAdoptionDataQueryPort;
+    private final LostAnimalEngineCommandPort lostAnimalEngineCommandPort;
 
     /**
      * 1. 데이터 ES_에 인덱싱
@@ -17,7 +29,29 @@ public class NotifyUsersOfSimilarLostPostsService implements NotifyUsersOfSimila
     @Override
     public void notifyUsersOfSimilarLostPosts(long id, PostType type, float[] embedding) {
         // 1. 데이터 ES_에 인덱싱
-        
+        // FOUND
+        if (type == PostType.FOUND) {
+            // 발견 게시물 데이터 가져오기
+            LostPost lostPost = lostPostDataQueryPort.findLostPostByIdOrThrow(id);
+
+            // lostPost -> lostAnimalDto
+            LostAnimalDto lostPostDto = LostAnimalDto.fromLostPost(lostPost, type, embedding);
+
+            // ES에 저장
+            lostAnimalEngineCommandPort.saveLostAnimalToEs(lostPostDto);
+        }
+
+        // FOSTER
+        if (type == PostType.FOSTER) {
+            // 구조 API 데이터 가져오기
+            LostAdoption lostAdoption = lostAdoptionDataQueryPort.findAdoptionById(id);
+
+            // lostAdoption -> lostAnimalDto
+            LostAnimalDto lostAdoptionDto = LostAnimalDto.fromLostAnimal(lostAdoption, type, embedding);
+
+            // ES에 저장
+            lostAnimalEngineCommandPort.saveLostAnimalToEs(lostAdoptionDto);
+        }
         // 2. ES 검색
         
         // 3. 알림 호출
