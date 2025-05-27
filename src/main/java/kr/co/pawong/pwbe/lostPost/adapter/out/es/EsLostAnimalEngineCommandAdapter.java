f57c171425e8cfe18e9936157ddaf2b2f9502054
h0725j <guswldpqj1@naver.com>
2025-05-27 14:12:09 +0900

    [feat] lostAnimal es 저장 구현

---
+++ src/main/java/kr/co/pawong/pwbe/lostPost/adapter/out/es/EsLostAnimalEngineCommandAdapter.java
@@ -0,0 +1,39 @@
+package kr.co.pawong.pwbe.lostPost.adapter.out.es;
+
+import kr.co.pawong.pwbe.global.error.errorcode.CustomErrorCode;
+import kr.co.pawong.pwbe.global.error.exception.BaseException;
+import kr.co.pawong.pwbe.lostPost.adapter.out.es.document.LostAnimalDocument;
+import kr.co.pawong.pwbe.lostPost.application.port.out.LostAnimalEngineCommandPort;
+import kr.co.pawong.pwbe.lostPost.application.port.out.dto.LostAnimalDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class EsLostAnimalEngineCommandAdapter implements LostAnimalEngineCommandPort {
+
+    private final ElasticsearchOperations elasticsearchOperations;
+
+    private static final IndexCoordinates INDEX = IndexCoordinates.of("lost_animal");
+
+    @Override
+    public void saveLostAnimalToEs(LostAnimalDto lostAnimalDto) {
+        try {
+            if (lostAnimalDto == null){
+                return;
+            }
+
+            LostAnimalDocument document = LostAnimalDocument.from(lostAnimalDto);
+
+            elasticsearchOperations.save(document, INDEX);
+            log.info("LostAnimal ES 저장이 완료되었습니다: {}", lostAnimalDto.getLostAnimalId());
+        } catch (Exception e) {
+            log.error("LostAnimal ES 저장이 실패하였습니다.", e);
+            throw new BaseException(CustomErrorCode.ES_SAVE_ERROR);
+        }
+    }
+}
---
+++ src/main/java/kr/co/pawong/pwbe/lostPost/adapter/out/es/document/LostAnimalDocument.java
@@ -0,0 +1,79 @@
+package kr.co.pawong.pwbe.lostPost.adapter.out.es.document;
+
+import kr.co.pawong.pwbe.adoption.enums.SexCd;
+import kr.co.pawong.pwbe.adoption.enums.UpKindCd;
+import kr.co.pawong.pwbe.lostPost.application.port.out.dto.LostAnimalDto;
+import kr.co.pawong.pwbe.lostPost.domain.LostGeoPoint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.GeoPointField;
+import org.springframework.data.elasticsearch.core.geo.GeoPoint;
+
+@Slf4j
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(indexName = "lost_animal")
+public class LostAnimalDocument {
+    @Id
+    private String id; // 자동생성 id
+
+    @Field(type = FieldType.Keyword, name = "lostAnimalId")
+    private String lostAnimalId; // type + id
+
+    @Field(type = FieldType.Keyword, name = "rdbId")
+    private Long rdbId; // id
+
+    @Field(type = FieldType.Keyword, name = "type")
+    private String type; // LOST, FOUND, RESQUE
+
+    @Field(type = FieldType.Keyword, name = "upKindCd")
+    private UpKindCd upKindCd;
+
+    @Field(type = FieldType.Keyword, name = "sexCd")
+    private SexCd sexCd;
+
+    @Field(type = FieldType.Keyword, name = "city")
+    private String city;
+
+    @Field(type = FieldType.Keyword, name = "district")
+    private String district;
+
+    @GeoPointField
+    @Field(name = "geoPoint")
+    private GeoPoint geoPoint;
+
+    @Field(type = FieldType.Dense_Vector, dims = 512, name = "embedding")
+    private float[] embedding;
+
+    public static LostAnimalDocument from(LostAnimalDto lostAnimalDto) {
+        GeoPoint geoPoint = null;
+        if (lostAnimalDto.getLostGeoPoint() != null) {
+            LostGeoPoint customGeoPoint = lostAnimalDto.getLostGeoPoint();
+            geoPoint = new GeoPoint(
+                    customGeoPoint.getLatitude().doubleValue(),
+                    customGeoPoint.getLongitude().doubleValue()
+            );
+        }
+
+        return LostAnimalDocument.builder()
+                .lostAnimalId(lostAnimalDto.getLostAnimalId())
+                .rdbId(lostAnimalDto.getRdbId())
+                .type(lostAnimalDto.getType())
+                .upKindCd(lostAnimalDto.getUpKindCd())
+                .sexCd(lostAnimalDto.getSexCd())
+                .city(lostAnimalDto.getCity())
+                .district(lostAnimalDto.getDistrict())
+                .geoPoint(geoPoint)
+                .embedding(lostAnimalDto.getEmbedding())
+                .build();
+    }
+}
