a91362a48105c8e2fcba4c0f4916828f95ce2a95
Dingwoonee <slm0523@naver.com>
2025-05-28 16:20:21 +0900

    [feat] SSE 방식으로 변경

---
+++ src/main/java/kr/co/pawong/pwbe/lostPost/adapter/in/sse/LostPostIndexedEventListener.java
@@ -0,0 +1,38 @@
+package kr.co.pawong.pwbe.lostPost.adapter.in.sse;
+
+import java.io.IOException;
+import java.util.List;
+import kr.co.pawong.pwbe.lostPost.adapter.in.sse.event.LostPostIndexedEvent;
+import kr.co.pawong.pwbe.lostPost.application.port.in.SearchSimilarLostAnimalsUseCase;
+import kr.co.pawong.pwbe.lostPost.application.port.in.dto.LostPostCard;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Component
+@RequiredArgsConstructor
+public class LostPostIndexedEventListener {
+
+    private final SseEmitters emitters;
+
+    private final SearchSimilarLostAnimalsUseCase searchSimilarLostAnimalsUseCase;
+
+    @EventListener
+    public void onIndexed(LostPostIndexedEvent event) {
+        Long postId = event.getLostPostId();
+        List<LostPostCard> result = searchSimilarLostAnimalsUseCase.searchSimilarLostAnimals(
+                null, postId);
+
+        emitters.getAll(postId).forEach(emitter -> {
+            try {
+                emitter.send(SseEmitter.event()
+                        .name("similar-animals")
+                        .data(result));
+                emitter.complete();
+            } catch (IOException e) {
+                emitter.completeWithError(e);
+            }
+        });
+    }
+}
