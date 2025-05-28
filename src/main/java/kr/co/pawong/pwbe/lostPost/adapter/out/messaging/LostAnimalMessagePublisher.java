d723ae5bdd06b31a875ef469c4c64b2e3da6131a
Dingwoonee <slm0523@naver.com>
2025-05-28 17:46:11 +0900

    [fix] 필드 주입을 생성자 주입으로 변경

--- src/main/java/kr/co/pawong/pwbe/lostPost/adapter/out/messaging/LostAnimalMessagePublisher.java
+++ src/main/java/kr/co/pawong/pwbe/lostPost/adapter/out/messaging/LostAnimalMessagePublisher.java
@@ -1,32 +1,44 @@
 package kr.co.pawong.pwbe.lostPost.adapter.out.messaging;
 
-import kr.co.pawong.pwbe.global.config.KafkaTopicConfig;
 import kr.co.pawong.pwbe.infrastructure.messaging.application.port.in.PublishMessageUseCase;
 import kr.co.pawong.pwbe.lostPost.application.port.out.LostAnimalMessagePublishPort;
 import kr.co.pawong.pwbe.lostPost.application.port.out.dto.CreatedLostAnimalPublishDto;
 import kr.co.pawong.pwbe.lostPost.enums.PostType;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
-@RequiredArgsConstructor
 public class LostAnimalMessagePublisher implements LostAnimalMessagePublishPort {
 
-    @Value("${kafka.topic.lost-post-created}")
-    private String LOST_POST_CREATED_TOPIC;
-    @Value("${kafka.topic.rescued-animal-created}")
-    private String RESCUED_ANIMAL_CREATED_TOPIC;
+    private final String lostPostCreatedTopic;
+    private final String rescuedAnimalCreatedTopic;
 
     private final PublishMessageUseCase publishMessageUseCase;
 
+    public LostAnimalMessagePublisher(
+            PublishMessageUseCase publishMessageUseCase,
+            @Value("${kafka.topic.lost-post-created}") String lostPostCreatedTopic,
+            @Value("${kafka.topic.rescued-animal-created}") String rescuedAnimalCreatedTopic
+    ) {
+        this.publishMessageUseCase = publishMessageUseCase;
+        this.lostPostCreatedTopic = lostPostCreatedTopic;
+        this.rescuedAnimalCreatedTopic = rescuedAnimalCreatedTopic;
+    }
+
     @Override
     public void publishLostAnimalCreatedMessage(CreatedLostAnimalPublishDto message) {
+        publishCreatedMessageByPostType(message);
+    }
+
+    private void publishCreatedMessageByPostType(CreatedLostAnimalPublishDto message) {
         switch (message.type()) {
             case PostType.LOST
-                    -> publishMessageUseCase.publishMessage(LOST_POST_CREATED_TOPIC, message);
+                    -> publishMessageUseCase.publishMessage(lostPostCreatedTopic, message);
             case PostType.FOUND, PostType.FOSTER
-                    -> publishMessageUseCase.publishMessage(RESCUED_ANIMAL_CREATED_TOPIC, message);
+                    -> publishMessageUseCase.publishMessage(rescuedAnimalCreatedTopic, message);
+            default -> log.warn("unexpected type: {}", message.type());
         }
     }
 }
