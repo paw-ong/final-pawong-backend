43cefee733f6f8e5d18bf61c5b7c9263b4c3371b
Sungyeon <csy357@gmail.com>
2025-05-29 17:32:30 +0900

    [fix] Auth filter Exception 반환 로직 수정

--- src/main/java/kr/co/pawong/pwbe/global/security/error/AuthenticationExceptionTranslator.java
+++ src/main/java/kr/co/pawong/pwbe/global/security/error/AuthenticationExceptionTranslator.java
@@ -3,6 +3,7 @@ package kr.co.pawong.pwbe.global.security.error;
 import static kr.co.pawong.pwbe.global.error.errorcode.CustomErrorCode.ACCESS_TOKEN_INVALIDATE;
 import static kr.co.pawong.pwbe.global.error.errorcode.CustomErrorCode.TOKEN_INVALIDATE;
 
+import java.util.HashMap;
 import java.util.Map;
 import kr.co.pawong.pwbe.global.error.errorcode.ErrorCode;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
@@ -12,12 +13,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class AuthenticationExceptionTranslator {
 
-    private Map<Class<? extends AuthenticationException>, ErrorCode> map = Map.of();
+    private Map<Class<? extends AuthenticationException>, ErrorCode> map =  new HashMap<>();
+
+    public AuthenticationExceptionTranslator() {
+        map.put(InsufficientAuthenticationException.class, TOKEN_INVALIDATE);
+    }
 
     public ErrorCode translate(AuthenticationException ex) {
-        if(ex instanceof InsufficientAuthenticationException)
-            map.put(ex.getClass(), TOKEN_INVALIDATE);
-
         return map.getOrDefault(ex.getClass(), ACCESS_TOKEN_INVALIDATE);
     }
 }
