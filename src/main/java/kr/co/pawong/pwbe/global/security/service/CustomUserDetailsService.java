ea1bd077132d261811362fab9e9c126290abd107
Sungyeon <csy357@gmail.com>
2025-05-29 14:24:05 +0900

    [refactor] 불필요한 코드 삭제

--- src/main/java/kr/co/pawong/pwbe/global/security/service/CustomUserDetailsService.java
+++ src/main/java/kr/co/pawong/pwbe/global/security/service/CustomUserDetailsService.java
@@ -1,34 +1,25 @@
 package kr.co.pawong.pwbe.global.security.service;
 
-import static kr.co.pawong.pwbe.global.error.errorcode.CustomErrorCode.USERNAME_NOT_FOUND;
 
+import java.util.List;
 import kr.co.pawong.pwbe.global.security.dto.CustomUserDetails;
-import kr.co.pawong.pwbe.global.security.error.exception.FilterAuthenticationException;
-import kr.co.pawong.pwbe.user.adapter.out.jpa.entity.UserEntity;
-import kr.co.pawong.pwbe.user.adapter.out.jpa.repository.UserJpaRepository;
+import kr.co.pawong.pwbe.user.application.port.in.QueryUserDataUseCase;
+import kr.co.pawong.pwbe.user.domain.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UserJpaRepository userJpaRepository;
-
-
-    public CustomUserDetailsService(
-            UserJpaRepository userJpaRepository) {
-        this.userJpaRepository = userJpaRepository;
-    }
-
+    private final QueryUserDataUseCase queryUserDataUseCase;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity userEntity = userJpaRepository.findBySocialId(Long.valueOf(username))
-                .orElseThrow(() ->
-                        new FilterAuthenticationException(USERNAME_NOT_FOUND,
-                                "User not found with username: " + username));
-        return new CustomUserDetails(userEntity);
+        User user = queryUserDataUseCase.getUserBySocialId(Long.valueOf(username));
+        return new CustomUserDetails(user.getUserId(), user.getSocialId(), List.of());
     }
 
 }
