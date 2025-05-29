6410d08cb11150907a12a2e3fbf7c5cb3640d52f
Sungyeon <csy357@gmail.com>
2025-05-29 14:24:05 +0900

    [refactor] 불필요한 코드 삭제

--- src/main/java/kr/co/pawong/pwbe/global/security/dto/CustomUserDetails.java
+++ src/main/java/kr/co/pawong/pwbe/global/security/dto/CustomUserDetails.java
@@ -1,59 +1,25 @@
 package kr.co.pawong.pwbe.global.security.dto;
 
-import java.util.Collection;
 import java.util.List;
-import kr.co.pawong.pwbe.user.adapter.out.jpa.entity.UserEntity;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+@Getter
 public class CustomUserDetails implements UserDetails {
 
     private final Long userId;
-    private final Long socialId;
-    private final String username;
+    private final String username;  // 추후 email로 변경
     private final String password;
     private final List<? extends GrantedAuthority> authorities;
 
-
-    public CustomUserDetails(UserEntity userEntity) {
-        this.userId = userEntity.getUserId();
-        this.socialId = userEntity.getSocialId();
-        this.username = String.valueOf(this.socialId);
-        this.password = "";
-        this.authorities = List.of();
-    }
-
+    // TODO: Long socialId를 String username으로 변경 (username에는 email을 저장)
     public CustomUserDetails(Long userId, Long socialId, List<SimpleGrantedAuthority> list) {
         this.userId = userId;
-        this.socialId = socialId;
         this.username = String.valueOf(socialId);
         this.password = "";
         this.authorities = list;
     }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return this.authorities;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public Long getSocialId() {
-        return socialId;
-    }
-
 
 }
