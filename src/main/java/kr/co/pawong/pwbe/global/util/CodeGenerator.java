e2dd138db74b57114ad04ee1e01c7595b41e25e5
yongje <dldydwp9024@gmail.com>
2025-05-31 16:50:17 +0900

    [refactor] 임의 난수 방지를 위한 조건 추가

--- src/main/java/kr/co/pawong/pwbe/global/util/CodeGenerator.java
+++ src/main/java/kr/co/pawong/pwbe/global/util/CodeGenerator.java
@@ -8,8 +8,11 @@ public class CodeGenerator {
     private static final SecureRandom RNG = new SecureRandom();
 
     public static String generateCode() {
-        int num = RNG.nextInt(1_000_000); // 0부터 999,999 사이의 정수
-        return String.format("%06d", num); // 항상 6자리 문자열로 반환 (앞에 0 채움)
+        int num;
+        do {
+            num = RNG.nextInt(1_000_000);
+        } while (num == 0);
+        return String.format("%06d", num);
     }
 
 }
