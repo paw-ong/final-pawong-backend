e731216d4d20fe5ae8046ea0e7235049ebd91c29
yongje <dldydwp9024@gmail.com>
2025-05-31 17:52:37 +0900

    [refactor] 이메일 주소, 인증코드가 url에 노출되지 않게 RequestBody로 수정

--- src/main/java/kr/co/pawong/pwbe/user/adapter/in/api/EmailController.java
+++ src/main/java/kr/co/pawong/pwbe/user/adapter/in/api/EmailController.java
@@ -1,12 +1,14 @@
 package kr.co.pawong.pwbe.user.adapter.in.api;
 
 import kr.co.pawong.pwbe.notification.application.port.in.MailUseCase;
+import kr.co.pawong.pwbe.user.adapter.in.api.dto.request.EmailVerificationRequest;
 import kr.co.pawong.pwbe.user.application.port.in.QueryUserDataUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,9 +29,12 @@ public class EmailController {
     }
 
     // 인증코드 검증
-    @GetMapping("/verifications")
-    public ResponseEntity<Boolean> verificationEmail(@RequestParam("email") String email,
-            @RequestParam("code") String authCode) {
+    @PostMapping("/verifications")
+    public ResponseEntity<Boolean> verificationEmail(@RequestBody EmailVerificationRequest request) {
+
+        String email = request.getEmail();
+        String authCode = request.getAuthCode();
+
         Boolean response = mailUseCase.verifiedCode(email, authCode);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
