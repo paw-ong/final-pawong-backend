ad4be5383f5a0a37c1ed795995856c26528b06a6
Lee, Yong-Je <155489671+Lee-Yongje@users.noreply.github.com>
2025-06-05 09:39:18 +0900

    [feat] status가 'MISSING'인 LostAdoptionDetail를 분리 (#110)

--- src/main/java/kr/co/pawong/pwbe/lostPost/application/port/in/QueryLostAdoptionDataUseCase.java
+++ src/main/java/kr/co/pawong/pwbe/lostPost/application/port/in/QueryLostAdoptionDataUseCase.java
@@ -1,12 +1,13 @@
 package kr.co.pawong.pwbe.lostPost.application.port.in;
 
+import kr.co.pawong.pwbe.lostPost.adapter.in.api.dto.response.LostAdoptionDetailResponse;
 import kr.co.pawong.pwbe.lostPost.application.port.in.dto.LostAdoptionDetailDto;
 import kr.co.pawong.pwbe.lostPost.application.port.in.dto.SliceLostPostSearchResponses;
 import org.springframework.data.domain.Pageable;
 
 public interface QueryLostAdoptionDataUseCase {
 
-    LostAdoptionDetailDto findAdoptionById(Long adoptionId);
-
     SliceLostPostSearchResponses fetchSlicedLostAdoptions(Pageable pageable, Long userId);
+
+    LostAdoptionDetailResponse getLostAdoptionDetails(Long adoptionId);
 }
